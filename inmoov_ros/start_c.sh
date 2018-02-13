8998fb16ce501b2202f1e4e4eb03911e23f1eab8
creos92 <33228553+creos92@users.noreply.github.com>
2018-02-13 20:21:11 +0100

    Update start_c.sh

--- inmoov_ros/start_c.sh
+++ inmoov_ros/start_c.sh
@@ -2,6 +2,7 @@
 source ./devel/setup.bash
 chmod +rx start_play.sh /Client/certificati/client1/start_client.sh
 cd /Client/certificati/client1 && ./start_client.sh &
+sleep 15s
 export ROS_IP=10.8.0.6
 export ROS_MASTER_URI=http://10.8.0.1:11311
 cd /inmoov_ros && ./start_play.sh
