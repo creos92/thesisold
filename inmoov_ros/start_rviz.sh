a37b2d15a3d3013f8e1b96bd0cb06995478cc8bb
creos92 <cesare.pec@gmail.com>
2018-02-13 18:52:43 +0100

    mart

--- inmoov_ros/start_rviz.sh
+++ inmoov_ros/start_rviz.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo Open Rviz
+sleep 5s
 source ./devel/setup.bash
-roslaunch inmoov_description display.launch model:=./src/inmoov_description/rob$
-
-
+roslaunch inmoov_description display.launch model:=./src/inmoov_description/robots/inmoov_right_hand.urdf.xacro 
