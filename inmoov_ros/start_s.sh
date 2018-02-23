bc0bd1a9263a7c3f2460aa4a089c41c5ae608441
creos92 <33228553+creos92@users.noreply.github.com>
2018-02-23 17:53:04 +0100

    Update start_s.sh

--- inmoov_ros/start_s.sh
+++ inmoov_ros/start_s.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 source ./devel/setup.bash
-chmod +rx ./start_rviz.sh ./start_talker.sh /Server/certificati/server/start_server.sh
+chmod +rx ./start_rviz.sh ./start_talker.sh /Server/certificati/server/start_server.sh &&
 cd /Server/certificati/server && ./start_server.sh &
+sleep 1s
 export ROS_IP=10.8.0.1
 export ROS_MASTER_URI=http://10.8.0.1:11311
 cd /inmoov_ros && ./start_rviz.sh & ./start_talker.sh
